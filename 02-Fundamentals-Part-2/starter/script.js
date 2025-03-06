9a374d00b112d318996d4e48183090649533a881
Tiago Rodrigues <tiagomgr97@gmail.com>
2025-03-06 11:52:12 +0000

    Lesson 40 done!

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -150,6 +150,7 @@ scoreKoalas = calcAverage(23, 34, 31);
 checkWinner(scoreDolphins, scoreKoalas)
 */
 
+/*
 const friend1 = 'Michael';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
@@ -187,3 +188,35 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages)
+*/
+
+
+// Add elementts
+const friends = ['Michael', 'Steven', 'Peter'];
+const newLenght = friends.push('Jay');
+console.log(friends);
+console.log("Lenght", newLenght);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped)
+console.log(friends);
+
+friends.shift(); //First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven!')
+}
