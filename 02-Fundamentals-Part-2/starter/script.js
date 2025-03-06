b71ede32dd8fe0a7e646900c0695ea7b7cbb42fb
Tiago Rodrigues <tiagomgr97@gmail.com>
2025-03-06 12:20:51 +0000

    Challenge 2 done!

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -190,7 +190,7 @@ const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length -
 console.log(ages)
 */
 
-
+/*
 // Add elementts
 const friends = ['Michael', 'Steven', 'Peter'];
 const newLenght = friends.push('Jay');
@@ -220,3 +220,14 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
     console.log('You have a friend called Steven!')
 }
+*/
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals)
