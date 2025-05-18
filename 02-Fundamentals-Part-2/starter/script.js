784720618aa001d65b0643ac82c6045a37906356
Tiago Rodrigues <tiagomgr97@gmail.com>
2025-05-18 19:49:24 +0100

    Updated!

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -254,19 +254,58 @@ const jonas = {
 console.log(jonas)
 */
 
+/*
+
 const jonas = {
     firstName: 'Tiago',
     lastName: 'Rodrigues',
     age: 2037 - 1991,
     job: 'Teacher',
-    firends: ['Michael', 'Peter', 'Steven']
+    friends: ['Michael', 'Peter', 'Steven'],
 };
 
-console.log(jonas);
+//console.log(jonas);
 
-console.log(jonas.lastName);
-console.log(jonas['lastName']);
+//console.log(jonas.lastName);
+//console.log(jonas['lastName']);
 
 const nameKey = 'Name';
-console.log(jonas['first' + nameKey]);
-console.log(jonas['last' + nameKey]);
+//console.log(jonas['first' + nameKey]);
+//console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends')
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request!Choose between firstName, lastName, age, job and friends.')
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasSschmedtam'
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best firend is
+// called Michael"
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`)
+
+*/
+
+
+const jonas = {
+    firstName: 'Tiago',
+    lastName: 'Rodrigues',
+    age: 2037 - 1991,
+    job: 'Teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function (birthYear) {
+        return 2037 - birthYear
+    }
+};
+
+console.log(jonas.calcAge(1991));
+console.log(jonas.calcAge(1991));
